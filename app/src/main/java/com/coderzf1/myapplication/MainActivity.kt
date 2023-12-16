7421cd8625807eb2f698c306bc02c877689823b4
TheRandomCrafter83 <williams.carl.83@gmail.com>
2023-12-16 18:08:41 -0500

    changed the base theme to Material3.DynamicColors

--- app/src/main/java/com/coderzf1/myapplication/MainActivity.kt
+++ app/src/main/java/com/coderzf1/myapplication/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.coderzf1.myapplication
 
+import android.graphics.drawable.VectorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.vectordrawable.graphics.drawable.VectorDrawableCompat
+import com.coderzf1.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
     }
 }
